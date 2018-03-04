6a14f6fa198918c40237ee350cd2b681a6420133
ElizavetaKovalchuk <elizkovalchuk13@gmail.com>
2018-03-04 14:51:22 +0300

    Add files via upload

---
+++ SerchOddElements.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+namespace DEV_2
+{
+    class SerchOddElements
+    {
+        public string enteredString;
+        public SerchOddElements(string enteredSymbol)
+        {
+            enteredString = enteredSymbol;
+        }
+        /// <summary>
+        /// Serch elements with odd ideks in a string
+        /// </summary>
+        /// <returns> string of elements with odd indeks</returns>
+        public string SerchOddSymbol()
+        {
+            StringBuilder oddElements = new StringBuilder(enteredString);
+            for (int i = 1; i < oddElements.Length; i++)
+            {
+                oddElements = oddElements.Remove(i,1);
+            }
+            string newString = oddElements.ToString();
+            return newString;
+        }
+    }
+}
