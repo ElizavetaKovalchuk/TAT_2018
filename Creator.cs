589ec2b6d4a6f522d937c87fdff7c1483b9bc6d0
ElizavetaKovalchuk <elizkovalchuk13@gmail.com>
2018-03-04 16:30:58 +0300

    Update and rename SerchOddElements.cs to Creator.cs

---
+++ Creator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+namespace DEV_2
+{
+    /// <summary>
+    /// Serch elements with odd indeks in a string and create string
+    /// </summary>
+    class Creator
+    {
+        public string sequnceOfSymbol;
+        public Creator(string sequnceOfSymbol)
+        {
+            sequnceOfSymbol = enteredSequence;
+        }
+        /// <summary>
+        /// Serch elements with odd ideks in a string and create string
+        /// </summary>
+        /// <returns> string of elements with odd indeks</returns>
+        public string CreateSequenceOfOddSymbol()
+        {
+            StringBuilder oddElements = new StringBuilder(sequnceOfSymbol);
+            for (int i = 1; i < oddElements.Length; i++)
+            {
+                oddElements = oddElements.Remove(i,1);
+            }
+            return oddElements.ToString();
+        }
+    }
+}
