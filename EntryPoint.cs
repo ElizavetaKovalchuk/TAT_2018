56b201a1b3a20726a77a13ec26d84d3180e9da25
ElizavetaKovalchuk <elizkovalchuk13@gmail.com>
2018-03-04 12:34:51 +0300

    Add files via upload

---
+++ EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace DEV_2
+{
+    class EntryPoin
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string:");
+            string line = Console.ReadLine();
+            StringBuilder sequence = new StringBuilder(line);
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                sequence = sequence.Remove(i,1);
+            }
+            Console.WriteLine(sequence);
+        }
+    }
+}
