7307ddaaa0eb64661dd4c213853b301a2dd2bd7f
ElizavetaKovalchuk <elizkovalchuk13@gmail.com>
2018-03-07 15:14:56 +0300

    Add files via upload

---
+++ DEV-3/DEV-3/EntryPoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace DEV_3
+{
+    /// <summary>
+    /// It is entry point of the program and it is print a number in the selected number system
+    /// </summary>
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            if (Int32.Parse(args[1]) >= 2 && Int32.Parse(args[1]) <= 20)
+            {
+                NumbersConversion enteredValues = new NumbersConversion(Int32.Parse(args[0]), Int32.Parse(args[1]));
+                foreach (var convertedNumber in enteredValues.ConversionToRadix())
+                {
+                    Console.WriteLine(convertedNumber);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DEV-3/DEV-3/NumbersConversion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DEV_3
+{
+    /// <summary>
+    /// Conversion a number to the selected number system
+    /// </summary>
+    class NumbersConversion
+    {
+        int number;
+        int scaleOfNotation;
+        public NumbersConversion (int arg0, int arg1)
+        {
+            number = arg0;
+            scaleOfNotation = arg1;
+        }
+
+        /// <summary>
+        /// Conversion a number to the selected number system
+        /// </summary>
+        /// <returns> Converted number</returns>
+        public List<int> ConversionToRadix()
+        {
+            List<int> convertedNumber = new List<int>();
+            int result = 0;
+            while (number > 0)
+            {
+                result = number % scaleOfNotation;
+                if (result <= 9)
+                {
+                    convertedNumber.Insert(0, result);
+                }
+                else
+                {
+                    char scale = (char)(65 + result - 10);
+                    convertedNumber.Insert(0, scale);
+                }
+                number /= scaleOfNotation;
+            }
+            return convertedNumber;
+        }
+    }
+}
